72ebb7cc232ab918585ac231e88197a387c98c02
acham1 <acham1@acham1-VirtualBox.(none)>
2016-07-27 00:05:06 -0700

    Completed Chapter 1 exercises.

---
+++ Chapter1/Exercise1-2.c
@@ -0,0 +1,12 @@
+/** Experiment to find out what happens when prints's argument string contains
+  * \c where c is some character not listed above. */
+
+#include <stdio.h>
+
+main() {
+	printf("Printing the \" character.\n");
+	printf("Printing the \\ character.\n");
+	printf("Printing the \' character.\n");
+	printf("Printing a tab \t.\n");
+	printf("Printing a new line. \n");
+}
