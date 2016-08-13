f8bd2a9928154e423fe1edc95067ff07778eee0c
acham1 <acham1@acham1-VirtualBox.(none)>
2016-08-12 22:02:52 -0700

    Completed up to Exercise1-16.c

---
+++ Chapter1/Exercise1-16.c
@@ -0,0 +1,59 @@
+/** Revise the main routine of the longest-line program so
+  * it will correctly print the length of arbitrary long input
+  * lines, and as much as possible of the text. */
+
+#include <stdio.h>
+#define MAXLINE 30		/* maximum input line length */
+
+int mygetline(char line[], int maxline);
+void copy(char from[], char to[]);
+
+main() {
+	int len;			/* current line length */
+	int max;			/* maximum length seen so far */
+	char line[MAXLINE + 1];	/* current input line */
+	char longest[MAXLINE + 1]; /* longest line saved here */
+
+	printf("Enter text to find greatest line length (newline included; max printable length is %d):\n", MAXLINE);
+
+	max = 0;
+	while ((len = mygetline(line, MAXLINE)) > 0) {
+		if (len > max) {
+			max = len;
+			copy(line, longest);
+		}
+	}
+	if (max > 0) {		/* there was a line */
+		printf("Longest line (length %d) received was:\n", max);
+		printf("%s\n", longest);
+	} else {
+		printf("No line received.\n");
+	}
+}  
+
+int mygetline(char line[], int maxline) {
+	int i = 0;
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF) {
+		if (i < maxline) {
+			line[i] = c;
+		}
+		i++;
+	}
+	if (c == '\n') {
+		if (i < maxline) {
+			line[i] = c;
+		}
+		i++;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+void copy(char from[], char to[]) {
+	int i = 0;
+	while((to[i] = from[i]) != '\0') {
+		i++;
+	}
+	to[i] = '\0';
+}
