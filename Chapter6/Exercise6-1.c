22f3596e37771591a44640dfd7e2697666292e7a
Alan VirtualBox <alanchamis@gmail.com>
2016-11-01 16:56:32 -0500

    Stub for Exercise6-1.c

--- Chapter6/Exercise6-1.c
+++ Chapter6/Exercise6-1.c
@@ -2,7 +2,22 @@
 #include "Chapter6.h"
 #include <stdlib.h>
 
+void print_heading(void);
+int getword(char *word, int lim) {
+    int c;
+    char *w = word;
+
+    while (isspace(c = getch())) {
+        
+    }
+}
+
 int main(void) {
     
     return EXIT_SUCCESS;
+}
+
+void print_heading(void) {
+    printf("Our version of getword does not properly handle udnerscores, string\n"
+        "constants, comments or preprocessor control lines. Write a better version.\n");
 }
