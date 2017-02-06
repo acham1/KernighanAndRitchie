7bb15dc50191b8cd03b29a42bc8d9517d5f5fc6c
acham1 <alanchamis@gmail.com>
2017-02-06 02:25:13 -0600

    Exercise6-6.c ready for testing

--- Chapter6/Exercise6-6.c
+++ Chapter6/Exercise6-6.c
@@ -7,6 +7,22 @@
 #define FILEINDEX 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* get next word from a char* and store in allocated memory */
+char * getWord(char* s) {
+    char temp[MAXLINE];
+    char* out;
+    int pos = 0; /* position to write to in temp */
+
+    while (!isspace(*s) && *s != '\0' && *s != EOF) {
+        temp[pos++] = *s++;
+    }
+    temp[pos] = '\0';
+    out = malloc(sizeof(char) * (1 + strlen(temp)));
+    return strcpy(out, temp);
+}
 
 struct nlist { /* table entry: */
     struct nlist *next; /* next entry in chain */
@@ -15,15 +31,6 @@ struct nlist { /* table entry: */
 };
 
 static struct nlist *hashtab[HASHSIZE]; /* pointer table */
-
-
-char *strdup(char *s) { /* make a duplicate of s */ 
-    char *p;
-    p = (char *) malloc(strlen(s)+1); /* +1 for '\0' */
-    if (p != NULL)
-        strcpy(p, s);
-    return p;
-}
 
 /** lookup: look for s in hashtab */
 struct nlist *lookup(char* s) {
@@ -36,21 +43,26 @@ struct nlist *lookup(char* s) {
     return NULL; /* not found */
 }
 
-/** lookup: look for s in hashtab */
+/** undef: remove s from hashtab 
+    returns: pointer to nlist that contains the removed string */
 struct nlist *undef(char* s) {
     struct nlist *np, *prev;
 
     prev = NULL;
     for (np = hashtab[hash(s)]; np != NULL; np = np->next) {
-        if (strcmp(s, np->name)) {
+        /* if find a match, then remove from table */
+        if (strcmp(s, np->name) == 0) {
             if (prev != NULL) {
                 prev->next = np->next;
             } else {
+                prev = np;
                 hashtab[hash(s)] = np->next;
             }
             return np; /* found */
+        } else {
+            /* if no match, advance to next node */
+            prev = np;
         }
-        prev = np;
     }
     return NULL; /* not found */
 }
@@ -60,7 +72,7 @@ unsigned hash(char *s) {
     for (hashval = 0; *s != '\0'; s++) {
         hashval = *s + 31 * hashval;
     }
-    return hashval % HASHSIZE;
+    return (unsigned) hashval % HASHSIZE;
 }
 
 /* install: put (name, defn) in hashtab */
@@ -84,19 +96,37 @@ unsigned hash(char *s) {
 }
 
 int main(int argc, char* argv[]) {
+    struct nlist* node;
     char tmpline[MAXLINE];
+    char* tableKey, *tableValue, *currentWord;
+    char* strPointer;
+    int containsDefine;
     FILE* f;
 
     f = fopen(argv[FILEINDEX]);
+    /* keep reading new lines until end of file */
     while (fgets(tmpline, MAXLINE, f) != NULL) {
-        if (first word is #define ) {
-            add to hashtab or ignore if already present
+        strPointer = strtok(tmpline, ' ');
+        containsDefine = strstr(strPointer, "#define");
+        /* if line starts with #define, then install entry in hashtable */
+        if (containsDefine == strPointer) {
+            strPointer = strtok(NULL, ' ');
+            tableKey = getWord(strPointer);
+            strPointer = strtok(NULL, ' ');
+            tableValue = getWord(strPointer);
+            install(tableKey, tableValue);
         } else {
-            while not end of line
-                if white space 
-                    print whitespace
-                else
-                    print next word
+            /* check if each word is in the dictionary, */
+            /* and print either the word of its definition*/
+            currentWord = getWord(strPointer);
+            while (strlen(currentWord)) {
+                if (node = lookup(currentWord)) {
+                    printf("%s ", node->defn);
+                } else {
+                    printf("%s ", currentWord);
+                }
+                free(currentWord);
+            }
         }
     }
 }
