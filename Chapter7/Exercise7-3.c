435c91af639a9442c81f8dd7e319685cec8cac88
acham1 <alanchamis@gmail.com>
2017-02-20 03:57:42 -0600

    Finished Exercise7-3.c

---
+++ Chapter7/Exercise7-3.c
@@ -0,0 +1,58 @@
+/** 
+ * Revise minprintf to handle more of the other facilities of printf
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void minprintf(char *fmt, ...) {
+    va_list ap;
+    char *p, *sval;
+    int ival;
+    double dval;
+
+    va_start(ap, fmt);
+    for (p = fmt; *p; p++) {
+        if (*p != '%') {
+            putchar(*p);
+            continue;
+        }
+        switch (*++p) {
+            case 'd':
+            case 'i':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                break;
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; sval++) {
+                    putchar(*sval);
+                }
+                break;
+            case 'o':
+                ival = va_arg(ap, int);
+                printf("%o", ival);
+                break;
+            case 'x':
+                ival = va_arg(ap, int);
+                printf("%x", ival);
+                break;
+            case 'X':
+                ival = va_arg(ap, int);
+                printf("%X", ival);
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+    va_end(ap);
+}
+
+int main(int argc, char* argv[]) {
+    return EXIT_SUCCESS;
+}
