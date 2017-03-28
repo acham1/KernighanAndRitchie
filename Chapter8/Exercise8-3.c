796716dc0efe5ccb1bca037e06e7c7edc01e2c36
Alan VirtualBox <alanchamis@gmail.com>
2017-03-28 00:43:43 -0500

    Finished Exercise8-3.c

--- Chapter8/Exercise8-3.c
+++ Chapter8/Exercise8-3.c
@@ -12,21 +12,21 @@
  * returns c, or EOF if error.
  */
 int _flushbuf(int c, FILE* stream) {
-    if (!stream->flags._WRITE || stream->flags._EOF || stream->flags._ERR) {
+    if (!stream->flag._WRITE || stream->flag._EOF || stream->flag._ERR) {
         return EOF;        
     }
     /* check if need to allocate the buffer first */
-    if (stream->base == NULL && !stream->flags._UNBUF) {
+    if (stream->base == NULL && !stream->flag._UNBUF) {
         stream->base = malloc(BUFSIZ);
         stream->ptr = stream->base;
         stream->cnt = BUFSIZ;
     }
     /* check if write to buffer or directly to file */
-    if (stream->flags._UNBUF) {
+    if (stream->flag._UNBUF) {
         stream->ptr = NULL;
         stream->base = NULL;
-        stream->count = 0;
-        return (write(stream->fd, (unsigned char*) &c) <= 0) ? EOF : c;
+        stream->cnt = 0;
+        return (write(stream->fd, (unsigned char*) &c, 1) <= 0) ? EOF : c;
     } else {
         /* number of chars written */
         int n = write(stream->fd, stream->base, stream->ptr - stream->base);
@@ -38,28 +38,29 @@ int _flushbuf(int c, FILE* stream) {
 }
 
 /* fflush: return EOF if error, otherwise return 0 */
-int fflush(File* stream) {
+int fflush(FILE* stream) {
     int status = 0;
     if (stream == NULL) {
-        for (File* f = _iob; f - _iob < OPEN_MAX; f++) {
-            status = (f->flags._WRITE && fflush(f) == EOF) ? -1 : status;
+        FILE* f;
+        for (f = _iob; f - _iob < OPEN_MAX; f++) {
+            status = (f->flag._WRITE && fflush(f) == EOF) ? -1 : status;
         }
     } else {
-        if (!stream->flags._WRITE) return EOF;
+        if (!stream->flag._WRITE) return EOF;
         _flushbuf(EOF, stream);
-        if (stream->flags._ERR) return -1;
+        if (stream->flag._ERR) return -1;
     }
     return status;
 }
 
 /* fclose: return 0 if successful, EOF otherwise */
-int fclose(File* stream) {
+int fclose(FILE* stream) {
     if (stream == NULL) {
         return EOF;
     }
     free(stream->base);
     stream->base = stream->ptr = NULL;
     stream->cnt = 0;
-    stream->flags = (_flag) {0,0,0,0,0};
-    return close(f->fd);
+    stream->flag = (_flags) {0,0,0,0,0};
+    return close(stream->fd);
 }
